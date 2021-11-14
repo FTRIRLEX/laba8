6032ceed83d0f734c8a3c72d417cdea0a4e4504a
FTRIPLEX <egorchik002@mail.ru>
2021-11-14 15:05:35 +0300

    lab8 fix 3.0

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -1,4 +1,5 @@
-import org.junit.Test;
+//import org.junit.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
